86e4804418f9ceef19f392eef4adeeff001f484e
Marco Fucci <marcofucci@gmail.com>
2016-12-08 13:44:54 +0000

    Rename Wagtail side navigation links
    This:
    
    - Renames "Explorer" to "Pages"
    - Adds an extra menu item "Dashboard" to the side navigation as
    clicking on the Wagtail logo is not very clear.

--- nhs_wagtailadmin/wagtail_hooks.py
+++ nhs_wagtailadmin/wagtail_hooks.py
@@ -1,12 +1,42 @@
+from django.core.urlresolvers import reverse
 from django.templatetags.static import static
-from django.utils.html import format_html
+from django.utils.html import format_html_join
+from django.utils.six import text_type
+from django.utils.translation import ugettext_lazy as _
+from wagtail.wagtailadmin.menu import MenuItem
 from wagtail.wagtailcore import hooks
 
 
 @hooks.register('insert_global_admin_js')
 def global_admin_js():
-    return format_html(
-        '<script src="{}"></script>'.format(
-            static('nhs_wagtailadmin/js/revision-preview.js')
-        )
+    return format_html_join(
+        '\n', '<script src="{0}"></script>',
+        [
+            (static('nhs_wagtailadmin/js/revision-preview.js'),),
+        ]
+    )
+
+
+@hooks.register('insert_global_admin_css')
+def global_admin_css():
+    return format_html_join(
+        '\n', '<link rel="stylesheet" href="{0}" type="text/css" />',
+        [
+            (static('nhs_wagtailadmin/css/nhs-overrides.css'),),
+        ]
+    )
+
+
+class DashboardMenuItem(MenuItem):
+    def is_active(self, request):
+        return request.path == text_type(self.url)
+
+
+@hooks.register('register_admin_menu_item')
+def register_explorer_menu_item():
+    return DashboardMenuItem(
+        _('Dashboard'), reverse('wagtailadmin_home'),
+        name='dashboard',
+        classnames='icon icon-wagtail',
+        order=50
     )
---
+++ nhs_wagtailadmin/wagtail_override_messages.py
@@ -0,0 +1,8 @@
+"""
+Used to override standard Wagtail messages
+"""
+from django.utils.translation import ugettext_lazy as _
+
+wagtail_standard_messages_to_override = [
+    _("Explorer"),
+]
