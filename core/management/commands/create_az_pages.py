8c6afb49468b86c9e39d8f50f9d92dc71eaf5d04
Marco Fucci <marcofucci@gmail.com>
2017-01-19 14:53:22 +0000

    Django command which creates A-Z empty pages
    Just meant to be used for user research and not production
    ready.
    
    Usage:
    
        ./manage.py create_az_pages

---
+++ core/management/commands/create_az_pages.py
@@ -0,0 +1,29 @@
+import json
+import os
+from django.core.management.base import BaseCommand
+
+
+from pages.models import EditorialPage, FolderPage
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        conditions_filepath = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'conditions.json'
+        )
+        with open(conditions_filepath, 'r') as f:
+            conditions = json.loads(f.read())
+
+            conditions_folder = FolderPage.objects.get(slug='conditions')
+
+            for condition in conditions:
+                new_page = EditorialPage(
+                    title=condition
+                )
+                conditions_folder.add_child(instance=new_page)
+
+                new_page.save_revision()
+
+        print('Finished...')
