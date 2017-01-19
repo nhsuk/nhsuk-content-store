4999322ca7755183e5e6d123ffa61830fd8b55d1
Marco Fucci <marcofucci@gmail.com>
2017-01-19 16:05:34 +0000

    Remove duplicates from conditions list

--- core/management/commands/create_az_pages.py
+++ core/management/commands/create_az_pages.py
@@ -1,7 +1,7 @@
 import json
 import os
-from django.core.management.base import BaseCommand
 
+from django.core.management.base import BaseCommand
 
 from pages.models import EditorialPage, FolderPage
 
