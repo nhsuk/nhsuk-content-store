90bfa8210ef9bf416f70465611264331ed0f4431
Marco Fucci <marcofucci@gmail.com>
2017-02-28 09:48:44 +0000

    Remove localization from draft|live action labels
    The verbose view draft, live action labels on page listings changed
    in https://github.com/wagtail/wagtail/pull/3304 so the code
    that overrode those is not needed any more.

--- nhs_wagtailadmin/wagtail_override_messages.py
+++ nhs_wagtailadmin/wagtail_override_messages.py
@@ -5,8 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 
 wagtail_standard_messages_to_override = [
     _("Explorer"),
-    _("Live"),
-    _("Draft"),
     _("Approve"),
     _("search description"),
 ]
